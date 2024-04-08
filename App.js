38bf55a16f4bcb67376ea4a2612e5aa22109e0ab
samue111l <samuelpblazquez@gmail.com>
2024-04-08 11:49:44 +0200

    integracion de firebase
    Npm install
    Dieron 5 vulnerabilidades
    
    Npx expo start
    No me dejo porque faltaban dependencias
    
    Npx expo install react-native-web react-dom
    Dieron 5 vulnerabilidades
    
    Npx expo start
    Funcionó la aplicación
    
    Npm install -save firebase reactfire@next
    Aparecieron 9 high vulnerabilidades
    
    Lo corregí con npm audit fix
    
    Hice la carpeta firebase-config.js
    
    Añadí firebaseProvider a app.js
    
    Npx expo install @expo/webpack(era necesario este paquete para que funcione con firebase)
    
    Creé un objeto firebase en inicio.js para comprobar que esta conectado

--- App.js
+++ App.js
@@ -2,9 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Inicio from './screens/inicio';
 
+import {
+  FirebaseAppProvider
+} from 'reactfire'
+import firebaseConfig from './firebase-config';
+import { Suspense } from 'react';
+
 export default function App() {
   return (
-    <Inicio />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Suspense fallback={'Conectando a la app...'}>
+        <Inicio />
+      </Suspense>
+    </FirebaseAppProvider>
+    
   );
 }
 
---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyAzVgUia8L2djpqHSyADEVi66h-joLqvw0",
+    authDomain: "fcts-80d66.firebaseapp.com",
+    projectId: "fcts-80d66",
+    storageBucket: "fcts-80d66.appspot.com",
+    messagingSenderId: "934677860410",
+    appId: "1:934677860410:web:85e4f8f9123b9335a5a503",
+    measurementId: "G-X18SQX3ZDD"
+  }
