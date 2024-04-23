339296a32fa3719fe5cd9bb67c2adff78bb58eca
12joseca12 <sanchezgarciajosecarlos12@gmail.com>
2024-04-24 01:59:24 +0200

    importacion-correcta-firebase
    He borrado lo anterior para importar de manera correcta firebase, se necesita java jdk 17 e instalar expo local. Añadidos las configuraciones de las apps ios y android y comienzo de logIn

--- App.js
+++ App.js
@@ -1,12 +1,4 @@
-import Inicio from './screens/inicio';
 import Nav from './componentes/Navegacion';
-
-
-import { FirebaseAppProvider} from 'reactfire'
-import {firebaseConfig} from './firebase-config';
-import { Suspense } from 'react';
-import Auth from './componentes/Auth/Auth';
-
 export default function App() {
    return (
         <Nav/> 
